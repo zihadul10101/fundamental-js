f7ee351021ce75e84801f1e06d7ed2f2a58586c7
unknown <you@example.com>
2021-02-15 17:47:59 +0600

    js core consept clear

--- 5-api-and-browser.js
+++ 5-api-and-browser.js
@@ -1,16 +1,33 @@
 //JSON
 const person={
     name:'Abdul',
-    age:25
+    age:25,
+    salary:500000
 };
 // console.log(JSON.stringify(person))
 //JSON convert back and forth
 const data=(JSON.stringify(person))
 const dataParsed=JSON.parse(data);
-console.log(dataParsed.name);
+//console.log(dataParsed.name);
 
 
 //API fetch
 // fetch('url')
 // .then(res=>res.json())
 // .then(data=>console.log(data));
+
+
+// local storage
+//localStorage.setItem('userId',1245);
+const userId = localStorage.getItem('userId');
+//console.log(userId);
+localStorage.setItem('person',JSON.stringify(person));
+const storedPerson=localStorage.getItem('person');
+const parsedPerson=JSON.parse(storedPerson);
+console.log(parsedPerson.age);
+
+const keys= Object.keys(parsedPerson);
+console.log(keys);
+
+const values=Object.values(parsedPerson);
+console.log(values);
